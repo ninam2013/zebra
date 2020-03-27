1d1472d4ff3ff538e5edac2819629dae1e95ff17
Evgeny Manin <ninam2013@gmail.com>
2020-03-27 20:50:48 +0500

    Адаптировал мобильное меню

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+$(document).ready(function () {
 var swiper = new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   });
+
+  $('.menu-icon').click(function () {
+    var $menuIcon = $(this);
+    $('.menu').slideToggle(500);
+    if ($menuIcon.hasClass('menu-icon_open')) {
+      $menuIcon.removeClass('menu-icon_open');
+    } else {
+      $menuIcon.addClass('menu-icon_open')
+    }   
+  });
+}); 
